e864a37684163c45ad719cd49c6021b3b9194c77
palla <pallab123ban@gmail.com>
2017-11-18 19:48:12 -0600

    inserted spring profiles and default profile.

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,19 +1,15 @@
 package guru.springframework.services;
 
-/**
- * @author palla
- */
-
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello!!! - Primary Greeting service";
+        return "Hallo!!! - Begrüßungsdienst";
     }
 }
